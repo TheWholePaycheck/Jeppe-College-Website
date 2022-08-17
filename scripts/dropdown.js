dcada566ef0e73eba4b63cc524d89c3832caab0b
sihle <siphesihlebomela@gmail.com>
2022-08-17 19:06:34 +0200

    🚚 create a dropdown script

---
+++ scripts/dropdown.js
@@ -0,0 +1,29 @@
+        // DROP DOWN FEATURE
+        const dropdown = document.querySelector(".dropdown")
+        let mouseIn = false
+
+        dropdown.addEventListener("mouseenter", () => {
+            mouseIn = true
+            dropdown.classList.add("drop")
+        })
+
+        dropdown.addEventListener("mouseleave", () => {
+            mouseIn = false
+            setTimeout(() => {
+                if (mouseIn == false) {
+                    dropdown.classList.remove("drop")
+                }
+            }, 350)
+        })
+
+        // MENU MOBILE DROPDOWN
+        const hamburger = document.querySelector(".hamburger") // select the hamburger
+        const closeIcon = document.querySelector(".close") // select the close icon
+        const menu = document.querySelector(".menu-mobile") // select the hamburger
+
+        // toggle classes when the mobile navigation icons are clicked
+        Array(hamburger, closeIcon).forEach(icon => {
+            icon.addEventListener("click", function () {
+                menu.classList.toggle("open") // add a class
+            })
+        })
