e6bdbcd6a51ab53e2410fa6236792be75e14f667
sihle <siphesihlebomela@gmail.com>
2022-08-23 17:25:32 +0200

    ✅ update chatbot

--- scripts/chatbot.js
+++ scripts/chatbot.js
@@ -2,7 +2,7 @@ var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
 (function () {
     var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
     s1.async = true;
-    s1.src = 'https://embed.tawk.to/62ea61d237898912e96103a0/1g9hos8oi';
+    s1.src = 'https://embed.tawk.to/6304f0fd54f06e12d8904b46/1gb5kqp4g';
     s1.charset = 'UTF-8';
     s1.setAttribute('crossorigin', '*');
     s0.parentNode.insertBefore(s1, s0);
